aa6b0f1bf7692c2b489bca249d9a3ea26510d0a9
junaidhkn <junaidhkn@gmail.com>
2023-07-05 00:59:57 +0500

    React Router Dom added

--- react_hooks/src/App.jsx
+++ react_hooks/src/App.jsx
@@ -1,8 +1,57 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import UseRef from "./components/UseRef.jsx";
+import Home from "./components/Home.jsx";
+import UseReducer from "./components/UseReducer.jsx";
+import UseMemo from "./components/UseMemo.jsx";
+import UseCallback from "./components/UseCallback.jsx";
+import UseId from "./components/UseId.jsx";
+import UseImperativeHandle from "./components/UseImperativeHandle.jsx";
+import UseLayoutEffect from "./components/UseLayoutEffect.jsx";
 
+const router = createBrowserRouter( [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/UseRef",
+    element: <UseRef />,
+  },
+  {
+    path: "/UseReducer",
+    element: <UseReducer />,
+  },
+  {
+    path: "/UseMemo",
+    element: <UseMemo />,
+  },
+  {
+    path: "/UseCallback",
+    element: <UseCallback />,
+  },
+  {
+    path: "/UseId",
+    element: <UseId />,
+  },
+  {
+    path: "/UseImperativeHandle",
+    element: <UseImperativeHandle />,
+  },
+  {
+    path: "/UseLayoutEffect",
+    element: <UseLayoutEffect />,
+  }
+] );
 
 const App = () => {
+
   return (
-    <div>App</div>
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
 
--- react_hooks/src/components/UseRef.jsx
+++ react_hooks/src/components/UseRef.jsx
@@ -1,8 +1,23 @@
+import { useState, useEffect, useRef } from "react";
 
 const UseRef = () => {
+   const [inputValue, setInputValue] = useState( "" );
+   const count = useRef( 0 );
+
+   useEffect( () => {
+      count.current = count.current + 1;
+   } );
+
    return (
-      <div>UseRef</div>
-   )
+      <>
+         <input
+            type="text"
+            value={inputValue}
+            onChange={( e ) => setInputValue( e.target.value )}
+         />
+         <h1>Render Count: {count.current}</h1>
+      </>
+   );
 }
 
 export default UseRef
