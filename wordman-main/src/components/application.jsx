fa2fc989561a684eefeadb191b2ff5bf3129bb8a
junaidhkn <junaidhkniazi@gmail.com>
2023-07-08 02:40:11 +0500

    Fixing Bug

--- wordman-main/src/components/application.jsx
+++ wordman-main/src/components/application.jsx
@@ -6,14 +6,14 @@ import GameInput from './game-input';
 import GameStatus from './game-status';
 
 const Application = () => {
-  const [colorGuess, setColorGuess] = useState('');
-  const [correctAnswer, setCorrectAnswer] = useState(generateRandomColor());
-  const [hasGuessed, setHasGuessed] = useState(false);
-  const [isWinner, setIsWinner] = useState(false);
+  const [colorGuess, setColorGuess] = useState( '' );
+  const [correctAnswer, setCorrectAnswer] = useState( () => generateRandomColor() );
+  const [hasGuessed, setHasGuessed] = useState( false );
+  const [isWinner, setIsWinner] = useState( false );
 
-  if (hasGuessed) {
-    if (correctAnswer === colorGuess) {
-      setIsWinner(true);
+  if ( hasGuessed ) {
+    if ( correctAnswer === colorGuess ) {
+      setIsWinner( true );
     }
   }
 
@@ -22,16 +22,16 @@ const Application = () => {
       <ColorSwatch color={correctAnswer} />
       <GameInput
         value={colorGuess}
-        onChange={(e) => setColorGuess(e.target.value)}
-        onSubmit={() => setHasGuessed(true)}
+        onChange={( e ) => setColorGuess( e.target.value )}
+        onSubmit={() => setHasGuessed( true )}
         disabled={hasGuessed}
       />
       <GameStatus isWinner={isWinner} hasGuessed={hasGuessed} />
       <button
         onClick={() => {
-          setCorrectAnswer(generateRandomColor());
-          setHasGuessed(false);
-          setColorGuess('');
+          setCorrectAnswer( generateRandomColor() );
+          setHasGuessed( false );
+          setColorGuess( '' );
         }}
         type={hasGuessed ? 'submit' : 'button'}
       >
